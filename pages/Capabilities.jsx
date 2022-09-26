d900bcda3d17d997c4565915072c180186e03f7a
Amir9eng <amirmukhtarme@gmail.com>
2022-09-26 10:51:44 +0100

    Management

--- pages/Capabilities.jsx
+++ pages/Capabilities.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Management from '../components/Management'
 import Navwhite from '../components/Navwhite'
+import Footer from './../components/Footer'
 
 const Capabilities = () => {
   return (
     <div>
       <Navwhite />
+      <Management />
+      <Footer />
     </div>
   )
 }
