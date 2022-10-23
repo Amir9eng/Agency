70500a680ebaf2a863b7760e3aa02b956fa5cca7
Amir9eng <amirmukhtarme@gmail.com>
2022-10-23 22:15:49 +0100

    projects link

--- components/Board.jsx
+++ components/Board.jsx
@@ -4,20 +4,22 @@ import SimpleFrame from './SimpleFrame'
 
 const Board = () => {
   return (
-    <section className='px-4 w-[289.01px] h-[449px] mx-auto mb-6 rounded-xl bg-gradient-to-r from-[#C7D2CF] to-[#E3F9FB]'>
+    <section className='px-4 h-[13.0625rem] w-[8.4375rem] md:w-[18.06rem] md:h-[28.06rem] mx-auto mb-6 rounded-xl bg-gradient-to-r from-[#C7D2CF] to-[#E3F9FB]'>
       <div className='py-10'>
         <Frame />
       </div>
       <main className='relative mt-4'>
-        <div className='absolute bottom-1 w-[68px] h-[76px] bg-[#A7D7E5] rounded-tr-xl rounded-tl-xl rounded-bl-xl'>
+        <div className='absolute bottom-1 w-[4.25rem] h-[4.75rem] bg-[#A7D7E5] rounded-tr-xl rounded-tl-xl rounded-bl-xl'>
           <p className='font-semibold text-center'>To Do</p>
         </div>
-        <div className='relative bg-[#313439] text-white w-[249.66px] h-[55.48px] rounded-xl px-4'>
-          <div className='absolute w-[12.9px] h-[12.9px] bg-[#A7D7E5] rounded-sm m-1.2 p-2 top-4 left-2'></div>
+        <div className='relative bg-[#313439] text-white w-[15.60375rem] h-[3.4675rem] rounded-xl px-4'>
+          <div className='absolute w-[0.80625rem] h-[0.80625rem] bg-[#A7D7E5] rounded-sm m-1.2 p-2 top-4 left-2'></div>
           <div className='ml-4'>
-            <p className='pt-1 text-[13px] '>QA Header Design for Home Page</p>
-            <aside className='bg-[#17A561] w-[90px] h-[14px] rounded-tr-full rounded-br-full  mt-1'>
-              <p className='text-[9.98px] px-1 font-semibold'>
+            <p className='pt-1 text-[0.8125rem] '>
+              QA Header Design for Home Page
+            </p>
+            <aside className='bg-[#17A561] w-[5.625rem] h-[.875rem] rounded-tr-full rounded-br-full  mt-1'>
+              <p className='text-[0.62375rem] px-1 font-semibold'>
                 Web & UI Design
               </p>
             </aside>
@@ -27,17 +29,17 @@ const Board = () => {
 
       <div className='mt-12'>
         <main className='relative mt-4'>
-          <div className='absolute bottom-1 w-[68px] h-[76px] bg-[#89F294] rounded-tr-xl rounded-tl-xl rounded-bl-xl'>
+          <div className='absolute bottom-1 w-[4.25rem] h-[4.75rem] bg-[#89F294] rounded-tr-xl rounded-tl-xl rounded-bl-xl'>
             <p className='font-semibold text-center'>To Do</p>
           </div>
-          <div className='relative bg-[#313439] text-white w-[249.66px] h-[55.48px] rounded-xl px-4'>
-            <div className='absolute w-[12.9px] h-[12.9px] bg-[#89F294] rounded-sm m-1.2 p-2 top-4 left-2'></div>
+          <div className='relative bg-[#313439] text-white w-[15.60375rem] h-[3.4675rem] rounded-xl px-4'>
+            <div className='absolute w-[0.80625rem] h-[0.80625rem] bg-[#89F294] rounded-sm m-1.2 p-2 top-4 left-2'></div>
             <div className='ml-4'>
-              <p className='pt-1 text-[13px] '>
+              <p className='pt-1 text-[0.8125rem] '>
                 QA Header Design for Home Page
               </p>
-              <aside className='bg-[#17A561] w-[90px] h-[14px] rounded-tr-full rounded-br-full  mt-1'>
-                <p className='text-[9.98px] px-1 font-semibold'>
+              <aside className='bg-[#17A561] w-[5.625rem] h-[.875rem] rounded-tr-full rounded-br-full  mt-1'>
+                <p className='text-[0.62375rem] px-1 font-semibold'>
                   Web & UI Design
                 </p>
               </aside>
--- components/Pricing.jsx
+++ components/Pricing.jsx
@@ -1,122 +1,201 @@
-import React from "react";
-import Image from "next/image";
-import Mark from "./Mark";
+import React from 'react'
+import Image from 'next/image'
+import Mark from './Mark'
 
 const Pricing = () => {
   return (
-    <section className="mx-auto py-6">
-      <h2 className="text-center my-4">Pricing</h2>
-      <aside className="flex lg:flex-row flex-col gap-y-4 gap-x-4 px-6 md:px-20">
-        <div className="bg-cyan-100 rounded-xl flex flex-col justify-between h-[49.6875rem] md:w-[19.6875rem] py-6 px-6 mx-auto">
-          <aside className="flex flex-col gap-y-2">
-            <p className="font-semibold text-2xl">Team</p>
-            <p className="font-semibold text-2xl">Augumentation</p>
+    <section className='mx-auto py-6'>
+      <h2 className='text-center my-4'>Pricing</h2>
+      <aside className='flex lg:flex-row flex-col gap-y-4 gap-x-4 px-6 md:px-20'>
+        <div className='bg-cyan-100 rounded-xl flex flex-col justify-between h-[49.6875rem] md:w-[19.6875rem] py-6 px-6 mx-auto'>
+          <aside className='flex flex-col gap-y-2'>
+            <p className='font-semibold text-2xl'>Team</p>
+            <p className='font-semibold text-2xl'>Augumentation</p>
             <small>What You’ll Get</small>
-            <p className="flex">
-              <Mark />
+            <p className='flex'>
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>
               Social Media Management on an hourly basis
             </p>
-            <p className="flex">
-              {" "}
-              <Mark /> Static social media{" "}
+            <p className='flex'>
+              {' '}
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>{' '}
+              Static social media{' '}
             </p>
-            <p className="flex">
-              {" "}
-              <Mark /> Unlimited Flexibility
+            <p className='flex'>
+              {' '}
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>{' '}
+              Unlimited Flexibility
             </p>
-            <p className="flex">
-              {" "}
-              <Mark /> Billed by hours invested at the beginning of each month
+            <p className='flex'>
+              {' '}
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>{' '}
+              Billed by hours invested at the beginning of each month
             </p>
-            <p className="flex">
-              {" "}
-              <Mark /> Deploy Our Team Whenever You Need To
+            <p className='flex'>
+              {' '}
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>{' '}
+              Deploy Our Team Whenever You Need To
             </p>
           </aside>
           <aside>
-            <div className="flex items-center my-1">
-              <p className="font-semibold text-2xl">A27$/</p>
-              <small className="mt-2">Hour</small>
+            <div className='flex items-center my-1'>
+              <p className='font-semibold text-2xl'>A27$/</p>
+              <small className='mt-2'>Hour</small>
             </div>
-            <button className="text-white px-24 py-3 rounded-xl bg-cyan-900 font-semibold">
+            <button className='text-white px-24 py-3 rounded-xl bg-cyan-900 font-semibold'>
               Choose
             </button>
           </aside>
         </div>
-        <div className="bg-cyan-100 rounded-xl h-[49.6875rem] md:w-[19.6875rem] py-6 px-6 flex flex-col justify-between">
+        <div className='bg-cyan-100 rounded-xl h-[49.6875rem] md:w-[19.6875rem] py-6 px-6 flex flex-col justify-between'>
           <aside>
             <h3>Standard</h3>
             <small>What You’ll Get</small>
-            <p className="flex">
-              <Mark /> 10 Hours of social media management per week
+            <p className='flex'>
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>{' '}
+              10 Hours of social media management per week
             </p>
-            <p className="flex">
-              {" "}
-              <Mark /> 2-3 Static Social Media Posts per week
+            <p className='flex'>
+              {' '}
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>{' '}
+              2-3 Static Social Media Posts per week
             </p>
-            <p className="flex">
-              {" "}
-              <Mark /> Full distribution and addaption to all platforms
+            <p className='flex'>
+              {' '}
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>{' '}
+              Full distribution and addaption to all platforms
             </p>
-            <p className="flex">
-              <Mark /> Complementary stories for all posts
+            <p className='flex'>
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>{' '}
+              Complementary stories for all posts
             </p>
-            <p className="flex">
-              <Mark /> Daily Community Management
+            <p className='flex'>
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>{' '}
+              Daily Community Management
             </p>
-            <p className="flex">
-              <Mark /> Daily access to social media manager via slack or other
-              platform
+            <p className='flex'>
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>{' '}
+              Daily access to social media manager via slack or other platform
             </p>
-            <p className="flex">
-              <Mark /> No Minimum Term
+            <p className='flex'>
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>{' '}
+              No Minimum Term
             </p>
           </aside>
           <aside>
-            <div className="flex items-center my-1">
-              <p className="font-semibold text-2xl">A700$/</p>
-              <small className="mt-2">month</small>
+            <div className='flex items-center my-1'>
+              <p className='font-semibold text-2xl'>A700$/</p>
+              <small className='mt-2'>month</small>
             </div>
-            <button className="text-white px-24 py-3 rounded-xl bg-cyan-900 font-semibold">
+            <button className='text-white px-24 py-3 rounded-xl bg-cyan-900 font-semibold'>
               Choose
             </button>
           </aside>
         </div>
-        <div className="bg-cyan-100 h-[49.6875rem] md:w-[19.6875rem] py-6 px-6 rounded-xl flex flex-col justify-between">
+        <div className='bg-cyan-100 h-[49.6875rem] md:w-[19.6875rem] py-6 px-6 rounded-xl flex flex-col justify-between'>
           <aside>
             <h3>Accellerated</h3>
             <small>What You’ll Get</small>
-            <p>20 Hours of social media management per week</p>
-            <p>3-5 Static and Motion Social Media Posts per week</p>
-            <p>Advanced Graphic Design & Motion Design Support</p>
-            <p>Paid Marketing and Post Boosting Support</p>
-            <p>Full distribution and addaption to all platforms</p>
-            <p>Daily Stories</p>
-            <p>Daily Community Management</p>
-            <p>
+            <p className='flex'>
+              {' '}
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>
+              20 Hours of social media management per week
+            </p>
+            <p className='flex'>
+              {' '}
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>
+              3-5 Static and Motion Social Media Posts per week
+            </p>
+            <p className='flex'>
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>
+              Advanced Graphic Design & Motion Design Support
+            </p>
+            <p className='flex'>
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>
+              Paid Marketing and Post Boosting Support
+            </p>
+            <p className='flex'>
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>
+              Full distribution and addaption to all platforms
+            </p>
+            <p className='flex'>
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>
+              Daily Stories
+            </p>
+            <p className='flex'>
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>
+              Daily Community Management
+            </p>
+            <p className='flex'>
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>
               Daily access to social media manager via slack or other platform
             </p>
-            <p>No Minimum Term</p>
+            <p className='flex'>
+              <span className='min-w-[18.9px] min-h-[18.9px]'>
+                <Mark />
+              </span>
+              No Minimum Term
+            </p>
           </aside>
           <aside>
-            <div className="flex items-center my-1">
-              <p className="font-semibold text-2xl">A1050$/</p>
-              <small className="mt-2">month</small>
+            <div className='flex items-center my-1'>
+              <p className='font-semibold text-2xl'>A1050$/</p>
+              <small className='mt-2'>month</small>
             </div>
-            <button className="text-white px-24 py-3 rounded-xl bg-cyan-900 font-semibold">
+            <button className='text-white px-24 py-3 rounded-xl bg-cyan-900 font-semibold'>
               Choose
             </button>
           </aside>
         </div>
-        <div className="bg-cyan-900 h-[49.6875rem] md:w-[19.6875rem] px-6 rounded-xl flex flex-col justify-between">
-          <div className="">
-            <p className="text-white text-2xl font-semibold mt-8">Custom</p>
-            <p className="text-white text-2xl font-semibold"> Deployment</p>
-            <p className="text-white my-4">
+        <div className='bg-cyan-900 h-[49.6875rem] md:w-[19.6875rem] px-6 rounded-xl flex flex-col justify-between'>
+          <div className=''>
+            <p className='text-white text-2xl font-semibold mt-8'>Custom</p>
+            <p className='text-white text-2xl font-semibold'> Deployment</p>
+            <p className='text-white my-4'>
               We will put together a bespoke plan for you based on the hours we
               will invest with your business to get your business what it needs.
             </p>
-            <button className="bg-cyan-100 rounded-xl text-cyan-900 px-24 py-3 font-semibold">
+            <button className='bg-cyan-100 rounded-xl text-cyan-900 px-24 py-3 font-semibold'>
               Choose
             </button>
           </div>
@@ -124,14 +203,14 @@ const Pricing = () => {
             <Image
               width={400.06}
               height={321.37}
-              alt="/"
-              src="/assets/media/Jacob.png"
+              alt='/'
+              src='/assets/media/Jacob.png'
             />
           </div>
         </div>
       </aside>
     </section>
-  );
-};
+  )
+}
 
-export default Pricing;
+export default Pricing
